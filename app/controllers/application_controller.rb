0cb95ba5b81e428b2d78ec5ee97d017114be9985
Benjamin <shallifi@hotmail.com>
2022-06-24 21:51:53 -0400

    completed Assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,38 @@ class ApplicationController < Sinatra::Base
     })
   end
 
+  get '/reviews' do
+    reviews = Review.all
+    reviews.to_json
+  end
+
+  post '/reviews' do
+    review = Review.create(
+      score: params[:score],
+      comment: params[:comment],
+      game_id: params[:game_id],
+      user_id: params[:user_id]
+    )
+    review.to_json
+  end
+
+  patch '/reviews/:id' do
+    reviews = Review.find(params[:id])
+    reviews.update(
+      score: params[:score],
+      comment: params[:comment]
+    )
+    reviews.to_json
+  end
+
+
+  delete '/reviews/:id' do
+    # find the review using the ID
+    review = Review.find(params[:id])
+    # delete the review
+    review.destroy
+    # send a response with the deleted review as JSON
+    review.to_json
+  end
+
 end
